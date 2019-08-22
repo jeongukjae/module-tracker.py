2ca67ff5f3f1572b38526214135fdf614dd33775
jeongukjae <JeongUkJae@gmail.com>
2019-08-23 02:58:47 +0900

    write track_glob function

--- module_tracker/tracker.py
+++ module_tracker/tracker.py
@@ -1,10 +1,20 @@
-import ast
 from glob import glob
 
+from .analyzer import parse_file
 
-class Tracker:
-    def __init__(self):
-        pass
 
-    def parse(self, directory: str):
-        pass
+def track_glob(glob_pattern: str):
+    modules = set()
+
+    for file_name in _get_files_from(glob_pattern):
+        modules.update(parse_file(file_name))
+
+    return sorted(list(modules))
+
+
+def track_directory(directory):
+    pass
+
+
+def _get_files_from(glob_pattern):
+    return glob(glob_pattern, recursive=True)
